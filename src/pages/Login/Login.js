93d275faf0cf31d963f0fc305caeb1316f9141ea
Scrappyz <ghostmic3000@gmail.com>
2023-11-21 19:09:21 +0800

    Added login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+    return (
+        <div class="Login-hGM">
+            <img class="logo-Pus" src="/assets/logo-aMF.png"/>
+            <p class="sign-in-to-website-name-HkM">Sign in to [Website Name]</p>
+            <div class="form-ktq">
+                <div class="email-4ed">
+                <p class="text-145">Email address</p>
+                <div class="shape-vwj">
+                </div>
+                </div>
+                <div class="password-qYu">
+                <p class="text-Ar5">Password</p>
+                <div class="shape-u2y">
+                </div>
+                </div>
+                <div class="sign-in-E5F">Sign in</div>
+            </div>
+            <div class="verify-3oP">
+                <span class="verify-3oP-sub-0">New User? </span>
+                <span class="verify-3oP-sub-1">Create an account</span>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
