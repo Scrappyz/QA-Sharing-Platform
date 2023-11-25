d116c4587ca25672058ab5edd13be0cf4294f7e1
Scrappyz <ghostmic3000@gmail.com>
2023-11-25 13:58:25 +0800

    update

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -22,57 +22,16 @@ function Home() {
                             <li className="recent-btn">Recent</li>
                         </ul>
                     </div>
-                    <div className="questions-c81">
+                    <div className="questions-container">
                         <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
-                        <div className="question-2-tWm">
-                        <div className="auto-group-mcxt-1bP">
-                            <div className="votes-Ldf">Votes</div>
-                            <div className="answers-fR3">Answers</div>
-                            <div className="views-Prq">Views</div>
-                        </div>
-                        <div className="auto-group-vk7s-jQu">
-                            <div className="question-2-GQq">Question 2</div>
-                            <div className="tags-zLq">Tags</div>
-                        </div>
-                        <div className="submitted-by--vVP">Submitted by: </div>
-                        </div>
-                        <div className="question-3-DjP">
-                        <div className="auto-group-nx29-8rM">
-                            <div className="votes-fbP">Votes</div>
-                            <div className="answers-CbK">Answers</div>
-                            <div className="views-Xdb">Views</div>
-                        </div>
-                    <div className="auto-group-utrb-UHw">
-                        <div className="question-3-cQ9">Question 3</div>
-                        <div className="tags-wxD">Tags</div>
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
+                        <Question votes={1261} answers={30} views={5230} question="What the dog doing?" tags={["beast"]} submittedBy="Jimbo" />
                     </div>
-                    <div className="submitted-by--sqs">Submitted by: </div>
-                    </div>
-                    <div className="question-4-ACh">
-                    <div className="auto-group-sjh7-H2R">
-                        <div className="votes-Q73">Votes</div>
-                        <div className="answers-WQy">Answers</div>
-                        <div className="views-SpR">Views</div>
-                    </div>
-                    <div className="auto-group-mqh3-n7b">
-                        <div className="question-4-Jrd">Question 4</div>
-                        <div className="tags-RgM">Tags</div>
-                    </div>
-                    <div className="submitted-by--ZGm">Submitted by: </div>
-                    </div>
-                    <div className="question-5-4UR">
-                    <div className="auto-group-qtqr-Pmb">
-                        <div className="votes-uV3">Votes</div>
-                        <div className="answers-3bF">Answers</div>
-                        <div className="views-BBf">Views</div>
-                    </div>
-                    <div className="auto-group-wpos-Jn5">
-                        <div className="question-5-2i5">Question 5</div>
-                        <div className="tags-ZCD">Tags</div>
-                    </div>
-                    <div className="submitted-by--V5s">Submitted by: </div>
-                    </div>
-                </div>
                 </div>
                 <div className="sidebar-right-Mtm">
                     <button className="ask-question" onClick={() => navigate("/askquestion")}>Ask Question</button>
