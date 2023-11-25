fc6fcf75791c37ea0bcb1ee5ff3db9932e5c07a6
Scrappyz <ghostmic3000@gmail.com>
2023-11-25 21:41:26 +0800

    Removed imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./views/pages/Home/Home.js";
-import Profile from "./views/pages/Profile/Profile.js";
-import AskQuestion from "./views/pages/AskQuestion/AskQuestion.js";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/askquestion" element={<AskQuestion />} />
             </Routes>
         </BrowserRouter>   
     )
