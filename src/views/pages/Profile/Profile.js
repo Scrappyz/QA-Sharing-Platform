a786963d75e78bfae4ce02f48f0c58491585f0f0
Scrappyz <ghostmic3000@gmail.com>
2023-12-02 20:58:08 +0800

    Removed unused css

--- src/views/pages/Profile/Profile.js
+++ src/views/pages/Profile/Profile.js
@@ -3,72 +3,7 @@ import "./Profile.css";
 
 function Profile() {
     return (
-        <div className="Profile-ayX">
-            <div className="header-KRK">
-                <p className="website-name-EoB">[Website Name]</p>
-                <div className="search-bar-LLR">Search</div>
-                <p className="Profile-Nnu">Profile</p>
-            </div>
-            <div className="auto-group-mge5-Uqw">
-                <div className="sidebar-left-p97">
-                <p className="home-iVP">Home</p>
-                <p className="users-EyX">Users</p>
-                <p className="saves-x8q">Saves</p>
-                </div>
-                <div className="main-G9X">
-                <div className="Profile-avu">
-                    <div className="Profile-pic-7R3">Picture</div>
-                    <div className="name-wey">
-                    <p className="your-name-GhF">Your Name</p>
-                    <p className="joined-on-month-day-year-7bP">Joined on [Month/Day/Year]</p>
-                    </div>
-                </div>
-                <div className="auto-group-h6d7-DuK">
-                    <div className="stats-BLM">
-                    <p className="stats-LU9">Stats</p>
-                    <div className="auto-group-cnk9-g2D">
-                        <div className="auto-group-rww3-CFT">
-                        <div className="questions-7NR">
-                            <p className="x-3Wy">[x]</p>
-                            <p className="questions-mSy">Questions</p>
-                        </div>
-                        <div className="upvotes-JSu">
-                            <p className="x-4BB">[x]</p>
-                            <p className="upvotes-yow">Upvotes</p>
-                        </div>
-                        </div>
-                        <div className="auto-group-zdaz-iFj">
-                        <div className="answers-fAy">
-                            <p className="x-oHB">[x]</p>
-                            <p className="answers-YEm">Answers</p>
-                        </div>
-                        <div className="upvotes-tJd">
-                            <p className="x-qUm">[x]</p>
-                            <p className="upvotes-yay">Upvotes</p>
-                        </div>
-                        </div>
-                        <div className="total-upvotes-Keq">
-                        <p className="x-mWq">[x]</p>
-                        <p className="total-upvotes-6Z7">Total Upvotes</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="information-nRw">
-                    <p className="information-XuK">Information</p>
-                    <div className="auto-group-jyau-U3s">
-                        <p className="full-name-Nuw">Full Name</p>
-                        <p className="age-u9B">Age</p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <div className="sidebar-right-BcV">
-                <p className="summary-JSD">Summary</p>
-                <p className="questions-EKs">Questions</p>
-                <p className="answers-y2Z">Answers</p>
-                </div>
-            </div>
-        </div>
+        <div className=""
     );
 }
 
