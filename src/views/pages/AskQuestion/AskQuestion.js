17e5fd58f1b68522657a83e8e568e3cb898ea149
Scrappyz <ghostmic3000@gmail.com>
2023-12-05 14:46:10 +0800

    Removed comments

--- src/views/pages/AskQuestion/AskQuestion.js
+++ src/views/pages/AskQuestion/AskQuestion.js
@@ -51,47 +51,6 @@ function AskQuestion() {
                 }} />
             </div>
         </div>
-        // <div className="AskQuestion-QFK">
-        //     <div className="main-ccH">
-        //         <div className="selection-F2y">
-        //         <div className="auto-group-eik3-9u3">
-        //             <div className="AskQuestion-4WD">Add Question</div>
-        //             <img className="line-2-j6Z" src="REPLACE_IMAGE:36:690"/>
-        //         </div>
-        //         <div className="line-1-ejK">
-        //         </div>
-        //         <div className="create-post-b8m">Create Post</div>
-        //         </div>
-        //         <div className="AskQuestion-WFj">Add Question</div>
-        //         <div className="title-container">
-        //         <div className="label">Title</div>
-        //         <div className="shape-qBX">
-        //         </div>
-        //         </div>
-        //         <div className="body-container">
-        //         <div className="label">Body</div>
-        //         <div className="buttons-container">
-        //             <img className="bold-Ecy" src="./assets/bold.png"/>
-        //             <img className="italic-Z9T" src="./assets/italic.png"/>
-        //             <img className="s-GpZ" src="./assets/s.png"/>
-        //             <img className="razorblade-Auw" src="./assets/razorblade.png"/>
-        //             <img className="numbering-Hjf" src="./assets/numbering.png"/>
-        //             <img className="bullet-pDo" src="./assets/bullet.png"/>
-        //             <img className="spacing-9G5" src="./assets/spacing.png"/>
-        //             <img className="undo-sC5" src="./assets/undo.png"/>
-        //             <img className="redo-zGh" src="./assets/redo.png"/>
-        //         </div>
-        //         <div className="text-body">
-        //         </div>
-        //         </div>
-        //         <div className="tags-container">
-        //         <div className="tags-xMw">Tags</div>
-        //         <div className="shape-gHw">
-        //         </div>
-        //         </div>
-        //         <div className="review-question-Py3">Review Question</div>
-        //    </div>
-        // </div>
     );
 }
 
