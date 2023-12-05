05644a9f1971c6d6d8b8e016e407d1bd6e467a09
Scrappyz <ghostmic3000@gmail.com>
2023-12-05 13:16:19 +0800

    reset

--- src/views/pages/AskQuestion/AskQuestion.js
+++ src/views/pages/AskQuestion/AskQuestion.js
@@ -4,64 +4,58 @@ import Header from "../../components/Header/Header.js";
 
 function AskQuestion() {
     return (
-        <div className="frame">
-            <Header />
-            <div className="main-container">
-
+        <div className="AskQuestion-QFK">
+            <div className="main-ccH">
+                <div className="selection-F2y">
+                <div className="auto-group-eik3-9u3">
+                    <div className="AskQuestion-4WD">Add Question</div>
+                    <img className="line-2-j6Z" src="REPLACE_IMAGE:36:690"/>
+                </div>
+                <div className="line-1-ejK">
+                </div>
+                <div className="create-post-b8m">Create Post</div>
+                </div>
+                <div className="AskQuestion-WFj">Add Question</div>
+                <div className="title-1TP">
+                <div className="title-v4Z">Title</div>
+                <div className="shape-qBX">
+                </div>
+                </div>
+                <div className="body-WHf">
+                <div className="body-RvR">Body</div>
+                <div className="body-buttons-kho">
+                    <img className="bold-Ecy" src="./assets/bold.png"/>
+                    <img className="italic-Z9T" src="./assets/italic.png"/>
+                    <img className="s-GpZ" src="./assets/s.png"/>
+                    <img className="razorblade-Auw" src="./assets/razorblade.png"/>
+                    <img className="numbering-Hjf" src="./assets/numbering.png"/>
+                    <img className="bullet-pDo" src="./assets/bullet.png"/>
+                    <img className="spacing-9G5" src="./assets/spacing.png"/>
+                    <img className="undo-sC5" src="./assets/undo.png"/>
+                    <img className="redo-zGh" src="./assets/redo.png"/>
+                </div>
+                <div className="text-body-76R">
+                </div>
+                </div>
+                <div className="tags-pmX">
+                <div className="tags-xMw">Tags</div>
+                <div className="shape-gHw">
+                </div>
+                </div>
+                <div className="review-question-Py3">Review Question</div>
+            </div>
+            <div className="sidebar-right-pYZ">Ask Question</div>
+            <div className="sidebar-left-f3P">
+                <p className="home-LQR">Home</p>
+                <p className="users-4bK">Users</p>
+                <p className="saves-o37">Saves</p>
+            </div>
+            <div className="header-JEm">
+                <p className="website-name-ztH">[Website Name]</p>
+                <div className="search-bar-Wbj">Search</div>
+                <p className="profile-9ub">Profile</p>
             </div>
         </div>
-        // <div className="AskQuestion-QFK">
-        //     <div className="main-ccH">
-        //         <div className="selection-F2y">
-        //         <div className="auto-group-eik3-9u3">
-        //             <div className="AskQuestion-4WD">Add Question</div>
-        //             <img className="line-2-j6Z" src="REPLACE_IMAGE:36:690"/>
-        //         </div>
-        //         <div className="line-1-ejK">
-        //         </div>
-        //         <div className="create-post-b8m">Create Post</div>
-        //         </div>
-        //         <div className="AskQuestion-WFj">Add Question</div>
-        //         <div className="title-1TP">
-        //         <div className="title-v4Z">Title</div>
-        //         <div className="shape-qBX">
-        //         </div>
-        //         </div>
-        //         <div className="body-WHf">
-        //         <div className="body-RvR">Body</div>
-        //         <div className="body-buttons-kho">
-        //             <img className="bold-Ecy" src="./assets/bold.png"/>
-        //             <img className="italic-Z9T" src="./assets/italic.png"/>
-        //             <img className="s-GpZ" src="./assets/s.png"/>
-        //             <img className="razorblade-Auw" src="./assets/razorblade.png"/>
-        //             <img className="numbering-Hjf" src="./assets/numbering.png"/>
-        //             <img className="bullet-pDo" src="./assets/bullet.png"/>
-        //             <img className="spacing-9G5" src="./assets/spacing.png"/>
-        //             <img className="undo-sC5" src="./assets/undo.png"/>
-        //             <img className="redo-zGh" src="./assets/redo.png"/>
-        //         </div>
-        //         <div className="text-body-76R">
-        //         </div>
-        //         </div>
-        //         <div className="tags-pmX">
-        //         <div className="tags-xMw">Tags</div>
-        //         <div className="shape-gHw">
-        //         </div>
-        //         </div>
-        //         <div className="review-question-Py3">Review Question</div>
-        //     </div>
-        //     <div className="sidebar-right-pYZ">Ask Question</div>
-        //     <div className="sidebar-left-f3P">
-        //         <p className="home-LQR">Home</p>
-        //         <p className="users-4bK">Users</p>
-        //         <p className="saves-o37">Saves</p>
-        //     </div>
-        //     <div className="header-JEm">
-        //         <p className="website-name-ztH">[Website Name]</p>
-        //         <div className="search-bar-Wbj">Search</div>
-        //         <p className="profile-9ub">Profile</p>
-        //     </div>
-        // </div>
     );
 }
 
