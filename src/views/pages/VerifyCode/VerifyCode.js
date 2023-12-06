3c6a57e2d21b50bdbbd620b1abdc01d582200cdc
Scrappyz <ghostmic3000@gmail.com>
2023-12-06 19:42:57 +0800

    Added logic

--- src/views/pages/VerifyCode/VerifyCode.js
+++ src/views/pages/VerifyCode/VerifyCode.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./VerifyCode.css";
 import { useNavigate } from "react-router-dom";
 
 function VerifyCode() {
     const navigate = useNavigate();
+    const [form, setForm] = useState({
+        code: ""
+    });
+
+    const handleChange = (e) => {
+        setForm((prev) => ({
+            ...prev, [e.target.name]: e.target.value
+        }));
+    }
+
+    const verify = () => {
+        if(form.code.length === 0) {
+            alert("[ERROR] Must provide verification code");
+            return;
+        } 
+
+        if(form.code.length !== 6 || isNaN(form.code)) {
+            alert("[ERROR] Code must be 6 digits");
+            return
+        }
+        
+        alert("[SUCCESS] You have been verified");
+        navigate("/");
+    }
 
     return (
         <div className="verify-code-frame">
@@ -12,9 +36,9 @@ function VerifyCode() {
             <div className="form-container">
                 <div className="verification-code">
                     <p className="label">Verification code</p>
-                    <input className="value" type="text" placeholder="XXXXXX"/>
+                    <input className="value" type="text" placeholder="XXXXXX" name="code" value={form.code} onChange={handleChange}/>
                 </div>
-                <button className="verify-btn" onClick={() => navigate("/")}>Verify</button>
+                <button className="verify-btn" onClick={verify}>Verify</button>
             </div>
             <div className="already-have-account">
                 <span>Already have an account?&nbsp;</span>
