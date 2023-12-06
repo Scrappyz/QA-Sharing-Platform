efb1986a1b5bb5c6c74734e3ade433f09da2dbc2
Scrappyz <ghostmic3000@gmail.com>
2023-12-06 16:00:12 +0800

    clears form on submit code

--- src/views/pages/Register/Register.js
+++ src/views/pages/Register/Register.js
@@ -1,37 +1,60 @@
 import React from "react";
 import "./Register.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Register() {
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        email: "",
+        username: "",
+        newPassword: "",
+        confirmPassword: ""
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setForm((prev) => ({
+            ...prev, [name]: value
+        }))
+    }
+
+    const sendCode = (e) => {
+        e.preventDefault();
+        setForm({
+            email: "",
+            username: "",
+            newPassword: "",
+            confirmPassword: ""
+        })
+    }
+
     return (
-        <div className="Register-edP">
-            <img className="logo-ALq" src="./assets/logo.png"/>
-            <p className="sign-up-to-our-website-name-HgM">Sign up to our [Website Name]</p>
-            <div className="form-C2d">
-                <div className="email-79b">
-                <p className="text-3ow">Email address</p>
-                <div className="shape-AtZ">
+        <div className="frame">
+            <img className="logo" src="./assets/logo.png"/>
+            <p className="label">Sign up to our [Website Name]</p>
+            <div className="form-container">
+                <div className="email">
+                    <p className="label">Email address</p>
+                    <input className="value" name="email" type="text" value={form.email} onChange={handleChange} />
                 </div>
+                <div className="username">
+                    <p className="label">Username</p>
+                    <input className="value" type="text" name="username" value={form.username} onChange={handleChange} />
                 </div>
-                <div className="username-tpZ">
-                <p className="text-dXF">Username</p>
-                <div className="shape-xZX">
+                <div className="new-password">
+                    <p className="label">New password</p>
+                    <input className="value" type="text" name="newPassword" value={form.newPassword} onChange={handleChange} />
                 </div>
+                <div className="confirm-password">
+                    <p className="label">Confirm password</p>
+                    <input className="value" type="text" name="confirmPassword" value={form.confirmPassword} onChange={handleChange} />
                 </div>
-                <div className="new-password-ruo">
-                <p className="text-1Xo">New password</p>
-                <div className="shape-La5">
-                </div>
-                </div>
-                <div className="confirm-password-EfT">
-                <p className="text-BKo">Confirm password</p>
-                <div className="shape-v2V">
-                </div>
-                </div>
-                <div className="send-code-dBo">Send code</div>
+                <button className="send-code" onClick={sendCode}>Send code</button>
             </div>
-            <div className="already-have-account-dLD">
-                <span className="already-have-account-dLD-sub-0">Already have an account? </span>
-                <span className="already-have-account-dLD-sub-1">Sign in</span>
+            <div className="already-have-account">
+                <span>Already have an account?&nbsp;</span>
+                <span className="txt" onClick={() => navigate("/login")}>Sign in</span>
             </div>
         </div>
     );
